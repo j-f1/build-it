15247f2360b41c4b4c8e9e3f9528b9b3a0571f86
Jed Fox <git@twopointzero.us>
2016-12-30 09:33:32 -0500

    🚀 Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // if (process.platform !== 'darwin') {
+  app.quit()
+  // }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
