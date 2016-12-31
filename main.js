c20b001d7097b22ddf94b0b53a2ddce01974c436
Jed Fox <git@twopointzero.us>
2016-12-31 14:07:06 -0500

    ➕ React DevTools

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron')
+const { REACT_DEVELOPER_TOOLS, REACT_PERF, default: installExtension } = require('electron-devtools-installer')
 
 let mainWindow
 
@@ -19,7 +20,12 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  Promise.all([REACT_DEVELOPER_TOOLS, REACT_PERF].map(installExtension))
+      .then((names) => names.map(name => console.log(`Added Extension:  ${name}`)))
+      .catch((err) => console.error('An error occurred: ', err))
+  createWindow()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
