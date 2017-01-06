c699f885db9eb4dc7f22ab6886ae59c420d2e8b4
Jed Fox <git@twopointzero.us>
2017-01-06 17:40:25 -0500

    Add animation when adding/removing issue types.

--- ui/js/status/issues.js
+++ ui/js/status/issues.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { st, vars, fa } from '../util'
+import TransitionGroup from 'react-transition-group/TransitionGroup'
+import { sineInOut } from 'eases'
 
 const icons = {
   warnings: 'warning',
@@ -25,14 +27,67 @@ export default function Issues (props) {
       }
     }
   })
-  return <div style={styles.container}>{Object.keys(issues).map(key => <Issue key={key} issue={issues[key]} type={key} style={styles.item} />)}</div>
+  return <TransitionGroup style={styles.container}>{Object.keys(issues).map(key => (issues[key].count === 0 || issues[key] === 0) ? null : <Issue key={key} issue={issues[key]} type={key} style={styles.item} />)}</TransitionGroup>
 }
 
-const Issue = st.handleActive((props) => {
-  const { issue } = props
-  if (issue.count === 0 || issue === 0) {
-    return null
+function animate (from, to, duration, cb) {
+  // cb(current, done)
+  const startTime = window.performance.now()
+  const diff = to - from
+  function _doWork () {
+    const dt = window.performance.now() - startTime
+    const current = from + diff * dt / duration
+    if (diff > 0
+      ? current < to
+      : current > to) {
+      cb(current, false)
+      window.requestAnimationFrame(_doWork)
+    } else {
+      cb(1, true)
+    }
   }
+  _doWork()
+}
+
+class Issue extends React.Component {
+  constructor (...args) {
+    super(...args)
+    this.state = {
+      progress: 0
+    }
+  }
+  componentWillAppear (cb) {
+    this.setState({ progress: 1 })
+  }
+  componentWillEnter (cb) {
+    this._animateTo(1, cb)
+  }
+  componentWillLeave (cb) {
+    this._animateTo(0, cb)
+  }
+  _animateTo (dest, cb, duration = 400) {
+    animate(this.state.progress, dest, duration, (progress, done) => {
+      this.setState({ progress })
+      if (done && cb) {
+        cb()
+      }
+    })
+  }
+  _animStyle () {
+    const progress = sineInOut(this.state.progress)
+    return {
+      opacity: Math.max(2 * (progress - 0.5), 0),
+      width: this._el ? this._el.getBoundingClientRect().width * progress : 'auto'
+    }
+  }
+  render () {
+    const { style, ...rest } = this.props
+    return <_IssueContent {...rest} style={Object.assign({}, style, this._animStyle())} ref_={el => { this._el = el }} />
+  }
+}
+
+const _IssueContent = st.handleActive((props) => {
+  const { issue } = props
   const styles = st({
     default: {
       icon: {
@@ -72,8 +127,8 @@ const Issue = st.handleActive((props) => {
       }
     }
   }, { active: props.active }, props.type)
-  return <a onClick={issue.click} style={props.style}>
+  return <span style={props.style}><a onClick={issue.click} ref={props.ref_}>
     <i style={styles.icon} className={fa(issue.icon || icons[props.type] || props.type)} />
     {' ' + (typeof issue.count === 'number' ? issue.count : issue)}
-  </a>
+  </a></span>
 })
