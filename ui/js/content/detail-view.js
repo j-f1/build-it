aaf74689a28bdc4ee40b92e351470a166bb05e47
Jed Fox <git@twopointzero.us>
2017-01-06 18:56:39 -0500

    Show colors in the warnings/errors pane.

--- ui/js/content/detail-view.js
+++ ui/js/content/detail-view.js
@@ -1,5 +1,25 @@
 import React from 'react'
+import RequestShortener from 'webpack/lib/RequestShortener.js'
+import Ansi from 'ansi-to-react'
+
+const shortener = new RequestShortener(process.cwd())
 
 export default function DetailView ({ data }) {
-  return data.content || <ul>{data.items.map((item, i) => <li key={i}><pre>{item.message}</pre></li>)}</ul>
+  if (data.content) {
+    return data.content
+  }
+  const items = {}
+  data.items.forEach((item) => {
+    const key = item.origin.readableIdentifier(shortener)
+    items[key] = (items[key] || []).concat(item.message.trim())
+  })
+  return <ul>
+    {Object.keys(items).map((name, i) => <li key={i}>
+      <strong>{name}</strong>
+      {items[name].length > 1
+        ? <ul>{items[name].map((msg, j) => <li key={j}><pre><Ansi>{msg}</Ansi></pre></li>)}</ul>
+        : <pre><Ansi>{items[name][0]}</Ansi></pre>
+      }
+    </li>)}
+  </ul>
 }
